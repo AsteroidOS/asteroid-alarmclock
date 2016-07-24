6e27aeb6dcae2268ac809c9e6dfe52dcac29f2d3
Egleson Beltran Quezada <eglesonquezada@gmail.com>
2016-07-24 15:19:24 +0000

    Added translation using Weblate (Spanish)

---
+++ asteroid-alarmclock.es.ts
@@ -0,0 +1,48 @@
+<?xml version='1.0' encoding='utf-8'?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es">
+<context>
+    <name>AlarmTimePickerDialog</name>
+    <message>
+        <location filename="AlarmTimePickerDialog.qml" line="31"></location>
+        <source>New Alarm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="AlarmTimePickerDialog.qml" line="31"></location>
+        <source>Edit Alarm</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AlarmViewRepeater</name>
+    <message>
+        <location filename="AlarmViewRepeater.qml" line="32"></location>
+        <source>Once</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="AlarmViewRepeater.qml" line="33"></location>
+        <source>Weekdays</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="AlarmViewRepeater.qml" line="34"></location>
+        <source>Weekends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="AlarmViewRepeater.qml" line="35"></location>
+        <source>Every day</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="67"></location>
+        <source>No alarms</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
