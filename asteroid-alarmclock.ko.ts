c6caea1465d4407a85ca0db9a49c5c3765f39681
snowmaan <gqbigpaps@gmail.com>
2016-12-09 08:49:49 +0000

    Added translation using Weblate (Korean)

---
+++ asteroid-alarmclock.ko.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ko">
+<context>
+    <name>AlarmTimePickerDialog</name>
+    <message>
+        <location filename="AlarmTimePickerDialog.qml" line="31"/>
+        <source>New Alarm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="AlarmTimePickerDialog.qml" line="31"/>
+        <source>Edit Alarm</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AlarmViewRepeater</name>
+    <message>
+        <location filename="AlarmViewRepeater.qml" line="32"/>
+        <source>Once</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="AlarmViewRepeater.qml" line="33"/>
+        <source>Weekdays</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="AlarmViewRepeater.qml" line="34"/>
+        <source>Weekends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="AlarmViewRepeater.qml" line="35"/>
+        <source>Every day</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="67"/>
+        <source>No alarms</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
