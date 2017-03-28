d6cb13c60385e3d87c9b9684fc6b45e29cd65375
Severi Hofslagare <zevez90@gmail.com>
2017-03-28 20:23:05 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (9 of 9 strings)

--- i18n/asteroid-alarmclock.fi.ts
+++ i18n/asteroid-alarmclock.fi.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../AlarmTimePickerDialog.qml" line="37"/>
         <source>New Alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>Aseta Hälytys</translation>
     </message>
     <message>
         <location filename="../AlarmTimePickerDialog.qml" line="37"/>
         <source>Edit Alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>Muokkaa Hälytystä</translation>
     </message>
 </context>
 <context>
@@ -19,22 +19,22 @@
     <message>
         <location filename="../AlarmViewRepeater.qml" line="32"/>
         <source>Once</source>
-        <translation type="unfinished"></translation>
+        <translation>Vain Kerran</translation>
     </message>
     <message>
         <location filename="../AlarmViewRepeater.qml" line="33"/>
         <source>Weekdays</source>
-        <translation type="unfinished"></translation>
+        <translation>Arkisin</translation>
     </message>
     <message>
         <location filename="../AlarmViewRepeater.qml" line="34"/>
         <source>Weekends</source>
-        <translation type="unfinished"></translation>
+        <translation>Viikonloppuisin</translation>
     </message>
     <message>
         <location filename="../AlarmViewRepeater.qml" line="35"/>
         <source>Every day</source>
-        <translation type="unfinished"></translation>
+        <translation>Joka Päivä</translation>
     </message>
 </context>
 <context>
@@ -42,7 +42,7 @@
     <message>
         <location filename="asteroid-alarmclock.desktop.h" line="5"/>
         <source>Alarm Clock</source>
-        <translation type="unfinished"></translation>
+        <translation>Herätyskello</translation>
     </message>
 </context>
 <context>
@@ -50,12 +50,12 @@
     <message>
         <location filename="../main.qml" line="59"/>
         <source>Alarms</source>
-        <translation type="unfinished"></translation>
+        <translation>Hälytykset</translation>
     </message>
     <message>
         <location filename="../main.qml" line="76"/>
         <source>No alarms</source>
-        <translation type="unfinished"></translation>
+        <translation>Ei Hälytyksiä</translation>
     </message>
 </context>
 </TS>
