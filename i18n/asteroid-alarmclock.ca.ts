ca257d9ccdd883c840f7c2d23e4a1747ed63f752
Marc Celeiro Escribà <maceleiro@gmail.com>
2017-04-24 18:39:09 +0000

    Translated using Weblate (Catalan)
    Currently translated at 100.0% (9 of 9 strings)

--- i18n/asteroid-alarmclock.ca.ts
+++ i18n/asteroid-alarmclock.ca.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../AlarmTimePickerDialog.qml" line="37"/>
         <source>Edit Alarm</source>
-        <translation>Editar l&apos;alarma</translation>
+        <translation>Editar l'alarma</translation>
     </message>
 </context>
 <context>
@@ -42,7 +42,7 @@
     <message>
         <location filename="asteroid-alarmclock.desktop.h" line="5"/>
         <source>Alarm Clock</source>
-        <translation type="unfinished"></translation>
+        <translation>Alarma</translation>
     </message>
 </context>
 <context>
@@ -50,7 +50,7 @@
     <message>
         <location filename="../main.qml" line="59"/>
         <source>Alarms</source>
-        <translation type="unfinished"></translation>
+        <translation>Alarmes</translation>
     </message>
     <message>
         <location filename="../main.qml" line="76"/>
