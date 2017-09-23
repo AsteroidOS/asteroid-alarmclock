6044864bfc96877bff5e6a2169f381b061bbbca8
Florent Revest <revestflo@gmail.com>
2017-09-23 07:57:11 +0000

    Added translation using Weblate (Norwegian Bokmål)

---
+++ alarmclock/i18n/asteroid-alarmclock.nb.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb">
+<context>
+    <name>AlarmTimePickerDialog</name>
+    <message>
+        <location filename="../AlarmTimePickerDialog.qml" line="37"/>
+        <source>New Alarm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AlarmTimePickerDialog.qml" line="37"/>
+        <source>Edit Alarm</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AlarmViewRepeater</name>
+    <message>
+        <location filename="../AlarmViewRepeater.qml" line="32"/>
+        <source>Once</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AlarmViewRepeater.qml" line="33"/>
+        <source>Weekdays</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AlarmViewRepeater.qml" line="34"/>
+        <source>Weekends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AlarmViewRepeater.qml" line="35"/>
+        <source>Every day</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="asteroid-alarmclock.desktop.h" line="5"/>
+        <source>Alarm Clock</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="59"/>
+        <source>Alarms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="76"/>
+        <source>No alarms</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
