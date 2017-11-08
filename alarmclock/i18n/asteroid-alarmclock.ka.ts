4aada66a80d6052d04c12064cb4ce5b119cd07af
sam bowlby <sam@chven.us>
2017-11-08 17:11:00 +0000

    Added translation using Weblate (Georgian)

---
+++ alarmclock/i18n/asteroid-alarmclock.ka.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>AlarmListItem</name>
+    <message>
+        <location filename="../AlarmListItem.qml" line="39"/>
+        <source>Once</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../AlarmListItem.qml" line="40"/>
+        <source>Weekdays</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../AlarmListItem.qml" line="41"/>
+        <source>Weekends</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../AlarmListItem.qml" line="42"/>
+        <source>Every day</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../AlarmListItem.qml" line="172"/>
+        <source>Add an alarm</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>DaysSelectorDialog</name>
+    <message>
+        <location filename="../DaysSelectorDialog.qml" line="36"/>
+        <source>Repetition</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="asteroid-alarmclock.desktop.h" line="5"/>
+        <source>Alarm Clock</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>TimePickerDialog</name>
+    <message>
+        <location filename="../TimePickerDialog.qml" line="38"/>
+        <source>Time</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
