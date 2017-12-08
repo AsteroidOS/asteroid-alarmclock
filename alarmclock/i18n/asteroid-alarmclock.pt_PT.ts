85a9572b2c779096023fae23e337a10bd35e5458
Pedro Silva <pedrenriques@gmail.com>
2017-12-08 02:16:13 +0000

    Translated using Weblate (Portuguese (Portugal))
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.pt_PT.ts
+++ alarmclock/i18n/asteroid-alarmclock.pt_PT.ts
@@ -26,7 +26,7 @@
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation></translation>
+        <translation>Adicionar um alarme</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
@@ -36,7 +36,7 @@
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation></translation>
+        <translation>Hora</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
