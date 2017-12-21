bbad87e9a8f65c31cc8705defdd4fbfacd073842
Andrea Scarpino <me@andreascarpino.it>
2017-12-21 07:17:14 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.it.ts
+++ alarmclock/i18n/asteroid-alarmclock.it.ts
@@ -26,17 +26,17 @@
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>Aggiungi sveglia</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
         <source>Repetition</source>
-        <translation type="unfinished"></translation>
+        <translation>Ripeti</translation>
     </message>
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Ora</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
