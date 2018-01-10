c35e128560a26ca43526d29cf2ad95216b60c0f8
TheMrRafus <themrrafus@gmail.com>
2018-01-10 08:46:24 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.es.ts
+++ alarmclock/i18n/asteroid-alarmclock.es.ts
@@ -26,7 +26,7 @@
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation>Agregar alarma</translation>
+        <translation>Añadir una alarma</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
