f765b3810f5d8f9af92b74383e220d2069f35be1
dom133 <dom133_pl@protonmail.com>
2018-04-09 18:01:19 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.pl.ts
+++ alarmclock/i18n/asteroid-alarmclock.pl.ts
@@ -36,7 +36,7 @@
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation>Godzina</translation>
+        <translation>Czas</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
