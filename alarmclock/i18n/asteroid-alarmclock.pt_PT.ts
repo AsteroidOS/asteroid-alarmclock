d81cc43e32d9c5fc8881d3411a01c28c29410fb5
Pedro Silva <pedrenriques@gmail.com>
2018-04-14 15:32:50 +0000

    Translated using Weblate (Portuguese (Portugal))
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.pt_PT.ts
+++ alarmclock/i18n/asteroid-alarmclock.pt_PT.ts
@@ -41,7 +41,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
         <source>Alarm Clock</source>
-        <translation>Despertador</translation>
+        <translation>Alarme</translation>
     </message>
 </context>
 </TS>
