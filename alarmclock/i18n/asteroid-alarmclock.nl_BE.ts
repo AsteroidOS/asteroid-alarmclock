5a07f30e5530a4ad262e8bf8cbd61941666361db
Nathan Follens <nthn@unseen.is>
2018-05-16 20:54:08 +0000

    Translated using Weblate (Flemish)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.nl_BE.ts
+++ alarmclock/i18n/asteroid-alarmclock.nl_BE.ts
@@ -6,12 +6,12 @@
     <message id="id-once">
         <location filename="../AlarmListItem.qml" line="41"/>
         <source>Once</source>
-        <translation></translation>
+        <translation>Eenmalig</translation>
     </message>
     <message id="id-weekdays">
         <location filename="../AlarmListItem.qml" line="43"/>
         <source>Weekdays</source>
-        <translation></translation>
+        <translation>Weekdagen</translation>
     </message>
     <message id="id-weekends">
         <location filename="../AlarmListItem.qml" line="45"/>
@@ -21,27 +21,27 @@
     <message id="id-every-day">
         <location filename="../AlarmListItem.qml" line="47"/>
         <source>Every day</source>
-        <translation></translation>
+        <translation>Dagelijks</translation>
     </message>
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation></translation>
+        <translation>Alarm toevoegen</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
         <source>Repetition</source>
-        <translation></translation>
+        <translation>Herhaling</translation>
     </message>
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation></translation>
+        <translation>Tijd</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
         <source>Alarm Clock</source>
-        <translation></translation>
+        <translation>Wekker</translation>
     </message>
 </context>
 </TS>
