aba4503038497967db588e48c0cffa2e6272f92e
Rodrigo Bernardi <rodrigobernardi@outlook.com.ar>
2018-05-16 22:37:25 +0000

    Translated using Weblate (Spanish (Argentina))
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.es_AR.ts
+++ alarmclock/i18n/asteroid-alarmclock.es_AR.ts
@@ -6,42 +6,42 @@
     <message id="id-once">
         <location filename="../AlarmListItem.qml" line="41"/>
         <source>Once</source>
-        <translation></translation>
+        <translation>Una vez</translation>
     </message>
     <message id="id-weekdays">
         <location filename="../AlarmListItem.qml" line="43"/>
         <source>Weekdays</source>
-        <translation></translation>
+        <translation>Días de la semana</translation>
     </message>
     <message id="id-weekends">
         <location filename="../AlarmListItem.qml" line="45"/>
         <source>Weekends</source>
-        <translation></translation>
+        <translation>Fines de semana</translation>
     </message>
     <message id="id-every-day">
         <location filename="../AlarmListItem.qml" line="47"/>
         <source>Every day</source>
-        <translation></translation>
+        <translation>Todos los días</translation>
     </message>
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation></translation>
+        <translation>Añadir una alarma</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
         <source>Repetition</source>
-        <translation></translation>
+        <translation>Repetición</translation>
     </message>
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation></translation>
+        <translation>Hora</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
         <source>Alarm Clock</source>
-        <translation></translation>
+        <translation>Alarma</translation>
     </message>
 </context>
 </TS>
