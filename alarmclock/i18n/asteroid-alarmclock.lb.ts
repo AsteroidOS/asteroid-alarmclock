e96d2a415023ad6a9fda4560917f7a875f84982f
Mika Beckerich <beckerichmika@gmail.com>
2018-05-17 15:46:21 +0000

    Translated using Weblate (Luxembourgish)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.lb.ts
+++ alarmclock/i18n/asteroid-alarmclock.lb.ts
@@ -6,42 +6,42 @@
     <message id="id-once">
         <location filename="../AlarmListItem.qml" line="41"/>
         <source>Once</source>
-        <translation></translation>
+        <translation>Eemol</translation>
     </message>
     <message id="id-weekdays">
         <location filename="../AlarmListItem.qml" line="43"/>
         <source>Weekdays</source>
-        <translation></translation>
+        <translation>Wochendeeg</translation>
     </message>
     <message id="id-weekends">
         <location filename="../AlarmListItem.qml" line="45"/>
         <source>Weekends</source>
-        <translation></translation>
+        <translation>Weekender</translation>
     </message>
     <message id="id-every-day">
         <location filename="../AlarmListItem.qml" line="47"/>
         <source>Every day</source>
-        <translation></translation>
+        <translation>All Dag</translation>
     </message>
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation></translation>
+        <translation>Wecker dobäimaachen</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
         <source>Repetition</source>
-        <translation></translation>
+        <translation>Widderhuelung</translation>
     </message>
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation></translation>
+        <translation>Zäit</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
         <source>Alarm Clock</source>
-        <translation></translation>
+        <translation>Wecker</translation>
     </message>
 </context>
 </TS>
