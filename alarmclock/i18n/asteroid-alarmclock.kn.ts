2732f395ba36dd81f6fc4e2ad25445d32ec372b3
DARREN MELROY MENEZES <dmm6799@gmail.com>
2018-05-20 14:02:50 +0000

    Translated using Weblate (Kannada)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.kn.ts
+++ alarmclock/i18n/asteroid-alarmclock.kn.ts
@@ -6,42 +6,42 @@
     <message id="id-once">
         <location filename="../AlarmListItem.qml" line="41"/>
         <source>Once</source>
-        <translation></translation>
+        <translation>ಒಮ್ಮೆ ( ಒಂದು ಸಲ)</translation>
     </message>
     <message id="id-weekdays">
         <location filename="../AlarmListItem.qml" line="43"/>
         <source>Weekdays</source>
-        <translation></translation>
+        <translation>ವಾರದ ದಿನಗಳು</translation>
     </message>
     <message id="id-weekends">
         <location filename="../AlarmListItem.qml" line="45"/>
         <source>Weekends</source>
-        <translation></translation>
+        <translation>ವಾರಾಂತ್ಯ</translation>
     </message>
     <message id="id-every-day">
         <location filename="../AlarmListItem.qml" line="47"/>
         <source>Every day</source>
-        <translation></translation>
+        <translation>ಎಲ್ಲಾ ದಿನಗಳು (ಯಾವಾಗಲೂ, ಯಾವತ್ತೂ)</translation>
     </message>
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation></translation>
+        <translation>ಎಚ್ಚರಿಕೆಯನ್ನು (ಎಲರಾಂ ಅನ್ನು) ಸೇರಿಸಿ</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
         <source>Repetition</source>
-        <translation></translation>
+        <translation>ಪುನರಾವರ್ತನೆ</translation>
     </message>
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation></translation>
+        <translation>ಸಮಯ</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
         <source>Alarm Clock</source>
-        <translation></translation>
+        <translation>ಎಚ್ಚರಿಕೆ ಗಂಟೆ (ಎಲರಾಂ)</translation>
     </message>
 </context>
 </TS>
