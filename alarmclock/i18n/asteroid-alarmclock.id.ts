010fe8372868cceeb452ac7a00b45494a86479b9
ditokp <ditokpl@gmail.com>
2018-11-24 10:21:02 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.id.ts
+++ alarmclock/i18n/asteroid-alarmclock.id.ts
@@ -6,42 +6,42 @@
     <message id="id-once">
         <location filename="../AlarmListItem.qml" line="41"/>
         <source>Once</source>
-        <translation></translation>
+        <translation>Sekali</translation>
     </message>
     <message id="id-weekdays">
         <location filename="../AlarmListItem.qml" line="43"/>
         <source>Weekdays</source>
-        <translation></translation>
+        <translation>Hari kerja</translation>
     </message>
     <message id="id-weekends">
         <location filename="../AlarmListItem.qml" line="45"/>
         <source>Weekends</source>
-        <translation></translation>
+        <translation>Hari libur</translation>
     </message>
     <message id="id-every-day">
         <location filename="../AlarmListItem.qml" line="47"/>
         <source>Every day</source>
-        <translation></translation>
+        <translation>Setiap hari</translation>
     </message>
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation></translation>
+        <translation>Tambahkan alarm</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
         <source>Repetition</source>
-        <translation></translation>
+        <translation>Pengulangan</translation>
     </message>
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation></translation>
+        <translation>Waktu</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
         <source>Alarm Clock</source>
-        <translation></translation>
+        <translation>Jam Alarm</translation>
     </message>
 </context>
 </TS>
