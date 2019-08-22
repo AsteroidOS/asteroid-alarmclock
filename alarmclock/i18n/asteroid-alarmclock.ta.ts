b53c9eb78ec2f01c22a5c611a3be2c263f6938c6
Khaleel Jageer <jskcse4@gmail.com>
2019-08-22 09:25:20 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.ta.ts
+++ alarmclock/i18n/asteroid-alarmclock.ta.ts
@@ -26,22 +26,22 @@
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>அலாரம் சேர்க்கவும்</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
         <source>Repetition</source>
-        <translation type="unfinished"></translation>
+        <translation>மீண்டும்</translation>
     </message>
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>நேரம்</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
         <source>Alarm Clock</source>
-        <translation type="unfinished"></translation>
+        <translation>ஒலிக்கடிகை</translation>
     </message>
 </context>
 </TS>
