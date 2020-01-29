8c42918e4455313c32dfba79794b6d4802742a15
XoSHnaW_2020_ <xoshnaw.betwata20@gmail.com>
2020-01-29 05:34:43 +0000

    Translated using Weblate (Sorani)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.ckb.ts
+++ alarmclock/i18n/asteroid-alarmclock.ckb.ts
@@ -11,7 +11,7 @@
     <message id="id-weekdays">
         <location filename="../AlarmListItem.qml" line="43"/>
         <source>Weekdays</source>
-        <translation type="unfinished"></translation>
+        <translation>هەموو ڕۆژێک لە ڕۆژەکانی هەفتە</translation>
     </message>
     <message id="id-weekends">
         <location filename="../AlarmListItem.qml" line="45"/>
@@ -26,22 +26,22 @@
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>زەنگێک زیاد بکە</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
         <source>Repetition</source>
-        <translation type="unfinished"></translation>
+        <translation>دووبارە بونەوە</translation>
     </message>
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>کات</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
         <source>Alarm Clock</source>
-        <translation type="unfinished"></translation>
+        <translation>زەنگی کاتژمێر</translation>
     </message>
 </context>
 </TS>
