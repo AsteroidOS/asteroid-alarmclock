c86eacf6a3e9feffa0b172e12633a2eb0c2cfd41
Sebastian <seby.nk91@gmail.com>
2020-05-30 15:30:23 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.ro.ts
+++ alarmclock/i18n/asteroid-alarmclock.ro.ts
@@ -6,42 +6,42 @@
     <message id="id-once">
         <location filename="../AlarmListItem.qml" line="41"/>
         <source>Once</source>
-        <translation type="unfinished"></translation>
+        <translation>O dată</translation>
     </message>
     <message id="id-weekdays">
         <location filename="../AlarmListItem.qml" line="43"/>
         <source>Weekdays</source>
-        <translation type="unfinished"></translation>
+        <translation>Zilele săptămânii</translation>
     </message>
     <message id="id-weekends">
         <location filename="../AlarmListItem.qml" line="45"/>
         <source>Weekends</source>
-        <translation type="unfinished"></translation>
+        <translation>Weekend-uri</translation>
     </message>
     <message id="id-every-day">
         <location filename="../AlarmListItem.qml" line="47"/>
         <source>Every day</source>
-        <translation type="unfinished"></translation>
+        <translation>În fiecare zi</translation>
     </message>
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>Adăugați o alarmă</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
         <source>Repetition</source>
-        <translation type="unfinished"></translation>
+        <translation>Repetare</translation>
     </message>
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Timp</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
         <source>Alarm Clock</source>
-        <translation type="unfinished"></translation>
+        <translation>Alarmă ceas</translation>
     </message>
 </context>
 </TS>
