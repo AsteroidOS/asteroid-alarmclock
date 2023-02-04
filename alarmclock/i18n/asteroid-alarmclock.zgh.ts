8faeb91e9e8f1caa6aad2f9d1174fb212310b323
ⵣⵓⵀⵉⵔ ⴰⵎⴰⵣⵉⵖ ZOUHIR DEHBI <zouhirdehbi56@gmail.com>
2023-02-04 21:05:51 +0000

    Translated using Weblate (Tamazight (Standard Moroccan))
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.zgh.ts
+++ alarmclock/i18n/asteroid-alarmclock.zgh.ts
@@ -6,42 +6,42 @@
     <message id="id-once">
         <location filename="../AlarmListItem.qml" line="41"/>
         <source>Once</source>
-        <translation type="unfinished"></translation>
+        <translation>ⵢⴰⵜ ⵜⵉⴽⴽⵍⵜ</translation>
     </message>
     <message id="id-weekdays">
         <location filename="../AlarmListItem.qml" line="43"/>
         <source>Weekdays</source>
-        <translation type="unfinished"></translation>
+        <translation>ⵓⵙⵙⴰⵏ ⵏ ⵉⵎⴰⵍⴰⵙⵙ</translation>
     </message>
     <message id="id-weekends">
         <location filename="../AlarmListItem.qml" line="45"/>
         <source>Weekends</source>
-        <translation type="unfinished"></translation>
+        <translation>ⵜⵉⵎⴳⴳⵓⵔⴰ ⵏ ⵉⵎⴰⵍⴰⵙⵙ</translation>
     </message>
     <message id="id-every-day">
         <location filename="../AlarmListItem.qml" line="47"/>
         <source>Every day</source>
-        <translation type="unfinished"></translation>
+        <translation>ⴽⵓ ⴰⵙⵙ</translation>
     </message>
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>ⵔⵏⵓ ⴽⵔⴰ ⵓⵙⴼⴰⴼⴰ</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
         <source>Repetition</source>
-        <translation type="unfinished"></translation>
+        <translation>ⴰⵍⴰⵙ</translation>
     </message>
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>ⴰⵣⵎⵣ</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
         <source>Alarm Clock</source>
-        <translation type="unfinished"></translation>
+        <translation>ⵜⵉⵏⴽⵔⵉ</translation>
     </message>
 </context>
 </TS>
