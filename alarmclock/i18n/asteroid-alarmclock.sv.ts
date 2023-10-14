ff35b22d0c284748a53cbb448cae53cc517b8569
P.O <rasmusson.mikael@protonmail.com>
2023-10-14 15:18:11 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.sv.ts
+++ alarmclock/i18n/asteroid-alarmclock.sv.ts
@@ -31,7 +31,7 @@
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
         <source>Repetition</source>
-        <translation>Repetition</translation>
+        <translation>Upprepa</translation>
     </message>
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
