9b2dafd37d75f92675c7f63f3f051b7fe18577a0
Toha <turok@duck.com>
2024-07-29 14:33:41 +0000

    Translated using Weblate (Belarusian)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.be.ts
+++ alarmclock/i18n/asteroid-alarmclock.be.ts
@@ -6,7 +6,7 @@
     <message id="id-once">
         <location filename="../AlarmListItem.qml" line="41"/>
         <source>Once</source>
-        <translation>адзін раз</translation>
+        <translation>Адзін раз</translation>
     </message>
     <message id="id-weekdays">
         <location filename="../AlarmListItem.qml" line="43"/>
