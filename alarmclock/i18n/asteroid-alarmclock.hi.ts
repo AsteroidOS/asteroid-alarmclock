3361226a6debc8cddb8fd3c88dbbaf83810d6e3d
Govind Gopal Yadav <govindgopalgaming@gmail.com>
2025-04-26 10:37:38 +0200

    Translated using Weblate (Hindi)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.hi.ts
+++ alarmclock/i18n/asteroid-alarmclock.hi.ts
@@ -16,17 +16,17 @@
     <message id="id-weekends">
         <location filename="../AlarmListItem.qml" line="45"/>
         <source>Weekends</source>
-        <translation>सप्ताहांत</translation>
+        <translation>सप्ताहांत/छुट्टी का दिन</translation>
     </message>
     <message id="id-every-day">
         <location filename="../AlarmListItem.qml" line="47"/>
         <source>Every day</source>
-        <translation>हर दिन</translation>
+        <translation>हर दिन / प्रतिदिन</translation>
     </message>
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation>नया अलार्म</translation>
+        <translation>नया अलार्म जोड़ें</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
