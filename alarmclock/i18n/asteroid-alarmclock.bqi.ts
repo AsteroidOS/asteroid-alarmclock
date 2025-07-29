369e4b449a767aed5c1039da201924247fae127e
hosêyň abāspanā <hosseinabaspanah@gmail.com>
2025-07-30 00:06:33 +0200

    Translated using Weblate (Luri (Bakhtiari))
    Currently translated at 87.5% (7 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.bqi.ts
+++ alarmclock/i18n/asteroid-alarmclock.bqi.ts
@@ -31,12 +31,12 @@
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
         <source>Repetition</source>
-        <translation type="unfinished"></translation>
+        <translation>تکرار</translation>
     </message>
     <message id="id-time">
         <location filename="../TimePickerDialog.qml" line="45"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>زمووݩ</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-alarmclock.desktop.h" line="6"/>
