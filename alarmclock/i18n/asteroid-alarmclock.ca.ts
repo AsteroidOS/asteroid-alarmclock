e54ab79f51b844e7739c43e5d5ec71e7d0fb346f
dtalens <databio@gmail.com>
2025-11-06 23:41:14 +0100

    Translated using Weblate (Catalan)
    Currently translated at 100.0% (8 of 8 strings)

--- alarmclock/i18n/asteroid-alarmclock.ca.ts
+++ alarmclock/i18n/asteroid-alarmclock.ca.ts
@@ -26,7 +26,7 @@
     <message id="id-add-alarm">
         <location filename="../AlarmListItem.qml" line="187"/>
         <source>Add an alarm</source>
-        <translation>Afegir una alarma</translation>
+        <translation>Afegeix una alarma</translation>
     </message>
     <message id="id-repetition">
         <location filename="../DaysSelectorDialog.qml" line="36"/>
